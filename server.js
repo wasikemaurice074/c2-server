80fd0898dbe616d3ad2c890a2b1fc86b46668e13
wasikemaurice074 <wasikemaurice074@gmail.com>
2026-06-01 16:58:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
